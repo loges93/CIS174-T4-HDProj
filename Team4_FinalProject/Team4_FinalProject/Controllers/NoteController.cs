d59913e7c0ee3ec9d0c1bdf1998d7ca2869017c0
clspurge <89756916+clspurge@users.noreply.github.com>
2021-12-13 20:32:55 -0600

    note controller, ticket controller updates; ticket, home, note view updates

--- Team4_FinalProject/Team4_FinalProject/Controllers/NoteController.cs
+++ Team4_FinalProject/Team4_FinalProject/Controllers/NoteController.cs
@@ -3,14 +3,62 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Team4_FinalProject.Models;
 
 namespace Team4_FinalProject.Controllers
 {
     public class NoteController : Controller
     {
-        public IActionResult Index()
+        private TicketManagerDbContext context { get; set; }
+        public NoteController(TicketManagerDbContext ctx) => context = ctx;
+
+        [HttpGet]
+        public ViewResult Add() => View("Add", new Note());
+
+        [HttpPost]
+        public IActionResult Add(Note n, int ticketid)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                
+                n.TicketId = ticketid;
+                n.DateCreated = DateTime.Now;
+                //n.TicketId = ticketId;
+                //ViewBag.TicketId = ticketId;
+                if (n.NoteId == 0)
+                    context.Notes.Add(n);
+                context.SaveChanges();
+                return RedirectToAction("Details", "Ticket");
+            }
+            else
+            {
+                return View();
+            }
         }
+        //[HttpGet]
+        //public IActionResult Add()
+        //{
+        //    Note n = new Note();
+        //    return View(n);
+        //}
+
+        //    [HttpPost]
+        //public IActionResult Add(int id)
+        //{
+
+        //    n.DateCreated = DateTime.Now;
+        //    n.TicketId = id;
+        //    if (ModelState.IsValid)
+        //    {
+        //        if (n.NoteId == 0)
+        //            context.Notes.Add(n);
+        //        context.SaveChanges();
+        //        return RedirectToAction("Detail", "Ticket");
+        //    }
+        //    else
+        //    {
+        //        //return RedirectToAction("Detail", "Ticket");
+        //        return View(n);
+        //    }
     }
 }
